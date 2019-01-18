0837e47cda15cb64f7ea23012ffa952f1608e963
Blake-E <mbe083@gmail.com>
2019-01-18 15:17:49 -0600

    pushing to heroku

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,36 +1,33 @@
 var friends = [
     {
-      name: "Ahmed",
-      photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-      scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+      name: "Ben Affleck",
+      photo: "http://www.gstatic.com/tv/thumb/persons/7530/7530_v9_ba.jpg",
+      scores: ["5", "4", "4", "2", "3", "3", "4", "1", "2", "4"]
     },
     {
-      name: "Jacob Deming",
-      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-      scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
+      name: "Lucy Lawless",
+      photo: "http://www.gstatic.com/tv/thumb/persons/67897/67897_v9_ba.jpg",
+      scores: ["3", "2", "1", "5", "1", "5", "4", "4", "5", "4"]
     },
     {
-      name: "Jeremiah Scanlon",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-      scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+      name: "Channing Tatum",
+      photo: "http://www.gstatic.com/tv/thumb/persons/321547/321547_v9_ba.jpg",
+      scores: ["5", "1", "4", "3", "2", "1", "5", "1", "3", "4"]
     },
     {
-      name: "Louis T. Delia",
-      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-      scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+      name: "Dolly Parton",
+      photo: "http://www.gstatic.com/tv/thumb/persons/1319/1319_v9_ba.jpg",
+      scores: ["3", "5", "1", "1", "1", "1", "1", "1", "5", "4"]
     },
     {
-      name: "Lou Ritter",
-      photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+      name: "Brad Pitt",
+      photo: "http://www.gstatic.com/tv/thumb/persons/1366/1366_v9_bb.jpg",
+      scores: ["4", "5", "5", "2", "4", "3", "1", "5", "4", "3"]
     },
     {
-      name: "Jordan Biason",
-      photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-      scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
+      name: "Sofia Vergara",
+      photo: "http://www.gstatic.com/tv/thumb/persons/435166/435166_v9_bb.jpg",
+      scores: ["1", "4", "4", "5", "1", "3", "5", "1", "2", "3"]
     }
   ];
   
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,5 +5,30 @@ module.exports = function(app) {
     res.json(friends);
   });
 
-  app.post("/api/friends", function(req, res) {});
+  app.post("/api/friends", function(req, res) {
+     var newFriend = req.body
+     var bestFriend = {
+         name: "",
+         photo: "",
+         totaldif: Infinity
+     }
+     for (var i =0; i < friends.length; i++) {
+        var currentFriend = friends[i];
+        var dif = 0
+        console.log(currentFriend.name);
+
+        for (var j = 0; j < currentFriend.scores.length; j ++){
+            var currentFriendScore = currentFriend.scores[j];
+            var currentUserScore = newFriend.scores[j];
+            dif = Math.abs(currentUserScore - currentFriendScore);
+        }
+        if (dif < bestFriend.totaldif){
+            bestFriend.name = currentFriend.name
+            bestFriend.photo = currentFriend.photo
+            bestFriend.totaldif = dif
+        }  
+     }
+friends.push(newFriend)
+res.json(bestFriend)
+     });
 };
